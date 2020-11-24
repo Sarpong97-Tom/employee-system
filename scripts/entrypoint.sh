33cb9e70e08d9c5ef698e7f0d3e5299fdbca2dff
Sarpong97-Tom <sarpongthomas05@gmail.com>
2020-11-23 16:39:43 -0800

    fix

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,5 +5,6 @@ set -e
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py runserver
 
-uwsgi --socket :8000 --master --enable-threads --module employeeSystem.wsgi
+# uwsgi --socket :8000 --master --enable-threads --module employeeSystem.wsgi

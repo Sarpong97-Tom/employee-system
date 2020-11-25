70f167c934b74ec3a0acf55023abc368cd2f9f92
Sarpong97-Tom <sarpongthomas05@gmail.com>
2020-11-24 20:15:54 -0800

    push app

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,8 +3,8 @@
 set -e
 
 
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py createsuperuser --noinput
+# python manage.py createsuperuser --noinput
 
 uwsgi --socket :8000 --master --enable-threads --module employeeSystem.wsgi
